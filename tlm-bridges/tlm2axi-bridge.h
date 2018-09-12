40c57f1ea5808404d5d42d8405df48d3f289f96f
Francisco Iglesias <francisco.iglesias@xilinx.com>
2018-09-12 10:33:19 +0200

    tlm2axi: Refactor and add support for pipelined phases
    This splits out the various phases into disctinct functions
    so that we can run them in parallel in separate SC threads.
    
    This also adds support for various AXI specific features:
    * Set AxSize to the data bus width
    * Add support for multiple outstanding transactions
    * Propagate the genattr_extension transaction ID
    * Convert the TLM streaming width to and AXI4 burst type
    * Correct the number of beats calculation
    
    Signed-off-by: Francisco Iglesias <francisco.iglesias@xilinx.com>
    Signed-off-by: Edgar E. Iglesias <edgar.iglesias@xilinx.com>

--- tlm-bridges/tlm2axi-bridge.h
+++ tlm-bridges/tlm2axi-bridge.h
@@ -33,7 +33,8 @@
 #define D(x)
 
 template
-<class BOOL_TYPE, template <int> class ADDR_TYPE, int ADDR_WIDTH, template <int> class DATA_TYPE, int DATA_WIDTH>
+<class BOOL_TYPE, template <int> class ADDR_TYPE, int ADDR_WIDTH, template <int> class DATA_TYPE, int DATA_WIDTH,
+	int ID_WIDTH = 8>
 class tlm2axi_bridge
 : public sc_core::sc_module
 {
@@ -57,7 +58,7 @@ public:
 	sc_out<sc_bv<2> > awburst;
 	sc_out<sc_bv<3> > awsize;
 	sc_out<sc_bv<8> > awlen;
-	sc_out<bool > awid;
+	sc_out<sc_bv<ID_WIDTH> > awid;
 	sc_out<bool > awlock;
 
 	/* Write data channel.  */
@@ -73,7 +74,7 @@ public:
 	sc_out<BOOL_TYPE> bready;
 	sc_in<DATA_TYPE<2> > bresp;
 	sc_in<sc_bv<2> > buser;
-	sc_in<bool> bid;
+	sc_in<sc_bv<ID_WIDTH> > bid;
 
 	/* Read address channel.  */
 	sc_out<BOOL_TYPE> arvalid;
@@ -87,7 +88,7 @@ public:
 	sc_out<sc_bv<2> > arburst;
 	sc_out<sc_bv<3> > arsize;
 	sc_out<sc_bv<8> > arlen;
-	sc_out<bool > arid;
+	sc_out<sc_bv<ID_WIDTH> > arid;
 	sc_out<bool > arlock;
 
 	/* Read data channel.  */
@@ -96,19 +97,346 @@ public:
 	sc_in<DATA_TYPE<DATA_WIDTH> > rdata;
 	sc_in<ADDR_TYPE<2> > rresp;
 	sc_in<sc_bv<2> > ruser;
-	sc_in<bool> rid;
+	sc_in<sc_bv<ID_WIDTH> > rid;
 	sc_in<bool> rlast;
+
+	static const uint32_t DATA_BUS_BYTES = DATA_WIDTH/8;
+
+	class Transaction
+	{
+	public:
+		Transaction(tlm::tlm_generic_payload& gp, sc_time& delay) :
+			m_gp(gp),
+			m_delay(delay)
+		{}
+
+		tlm::tlm_generic_payload& GetGP() { return m_gp; }
+
+		sc_time& GetDelay() { return m_delay; }
+
+		sc_event& DoneEvent() { return m_done; }
+	private:
+		tlm::tlm_generic_payload& m_gp;
+		sc_time& m_delay;
+		sc_event m_done;
+	};
+
+	sc_fifo<Transaction*> transFifo;
+
+	sc_fifo<Transaction*> rdRespFifo;
+
+	sc_fifo<Transaction*> wrDataFifo;
+	sc_fifo<Transaction*> wrRespFifo;
+
 
 private:
 	int prepare_wbeat(tlm::tlm_generic_payload& trans, sc_time& delay,
 			unsigned int offset);
+
 	virtual void b_transport(tlm::tlm_generic_payload& trans,
-					sc_time& delay);
+					sc_time& delay)
+	{
+		Transaction *tr = new Transaction(trans, delay);
+		transFifo.write(tr);
+
+		wait(tr->DoneEvent());
+
+		delete tr;
+	}
+
+	void read_address_phase(sc_dt::uint64 addr,
+					uint8_t burstType,
+					unsigned int prot,
+					unsigned int nr_beats,
+					uint32_t transaction_id)
+	{
+		araddr.write(addr);
+		arprot.write(prot);
+		arsize.write((DATA_WIDTH/8)/2);
+		arlen.write(nr_beats - 1);
+		arburst.write(burstType);
+		arid.write(transaction_id);
+
+		arvalid.write(true);
+
+		do {
+			wait(clk.posedge_event());
+		} while (arready.read() == false);
+
+		arvalid.write(false);
+	}
+
+	void write_address_phase(sc_dt::uint64 addr,
+					uint8_t burstType,
+					unsigned int prot,
+					unsigned int nr_beats,
+					uint32_t transaction_id)
+	{
+		awaddr.write(addr);
+		awprot.write(prot);
+		awsize.write((DATA_WIDTH/8)/2);
+		awlen.write(nr_beats - 1);
+		awburst.write(burstType);
+		awid.write(transaction_id);
+
+		awvalid.write(true);
+
+		do {
+			wait(clk.posedge_event());
+		} while (awready.read() == false);
+
+		awvalid.write(false);
+	}
+
+
+	enum { NumBurstWidths = 8 };
+
+	bool ValidateBurstWidth(uint32_t burst_width)
+	{
+		uint8_t AXIBurstWidths[NumBurstWidths] = { 1, 2, 4, 8, 16, 32, 64, 128 };
+
+		for (int i = 0; i < NumBurstWidths; i++) {
+			if (AXIBurstWidths[i] == burst_width) {
+				return true;
+			}
+		}
+
+		return false;
+	}
+
+	uint8_t GetNumBeats(tlm::tlm_generic_payload& gp)
+	{
+		uint64_t address = gp.get_address();
+		unsigned int dataLen = gp.get_data_length();
+		uint32_t burst_width = 0;
+		genattr_extension *genattr;
+		uint64_t alignedAddress;
+		uint8_t nrBeats;
+
+		gp.get_extension(genattr);
+		if (genattr) {
+			burst_width = genattr->get_burst_width();
+		}
+
+		if (burst_width == 0) {
+			// Default to databus width
+			burst_width = DATA_BUS_BYTES;
+		}
+
+		if (!ValidateBurstWidth(burst_width)) {
+			SC_REPORT_ERROR("tlm2axi", "AXI burst width error");
+		}
+
+		alignedAddress = (address / burst_width) * burst_width;
+		dataLen += address - alignedAddress;
+
+		nrBeats = dataLen/burst_width;
+
+		if (dataLen % burst_width) {
+			nrBeats++;
+		}
+
+		return nrBeats;
+	}
+
+	void address_phase()
+	{
+		while (true) {
+			Transaction *tr = transFifo.read();
+			tlm::tlm_generic_payload& trans = tr->GetGP();
+			unsigned int datalen = trans.get_data_length();
+			unsigned int streaming_width = trans.get_streaming_width();
+			genattr_extension *genattr;
+			unsigned int prot = 0;
+			uint8_t transaction_id = 0;
+			uint8_t burstType;
+
+			/* Extensions.  */
+			trans.get_extension(genattr);
+			if (genattr) {
+				if (genattr->get_non_secure()) {
+					prot |= AXI_PROT_NS;
+				}
+				transaction_id = genattr->get_transaction_id();
+			}
+
+			//
+			// Burst type
+			//
+			if (streaming_width >= datalen) {
+				burstType = AXI_BURST_INCR;
+			} else if (streaming_width == DATA_BUS_BYTES) {
+				burstType = AXI_BURST_FIXED;
+			} else if (streaming_width < DATA_BUS_BYTES) {
+
+				//
+				// Specify this with burst_width if streaming
+				// width is less than the data bus width
+				//
+
+				burstType = AXI_BURST_FIXED;
+
+				if (genattr == nullptr) {
+					genattr = new genattr_extension();
+					trans.set_extension(genattr);
+				}
+
+				genattr->set_burst_width(streaming_width);
+			} else {
+				burstType = AXI_BURST_WRAP;
+			}
+
+			/* Send the address.  */
+			if (trans.is_read()) {
+				read_address_phase(trans.get_address(),
+							burstType,
+							prot,
+							GetNumBeats(trans),
+							transaction_id);
+				rdRespFifo.write(tr);
+			} else {
+				write_address_phase(trans.get_address(),
+							burstType,
+							prot,
+							GetNumBeats(trans),
+							transaction_id);
+				wrDataFifo.write(tr);
+			}
+		}
+	}
+
+	void read_resp_phase()
+	{
+		rready.write(false);
+
+		while (true) {
+			Transaction *tr = rdRespFifo.read();
+			tlm::tlm_generic_payload& trans = tr->GetGP();
+			unsigned char *data = trans.get_data_ptr();
+			unsigned int len = trans.get_data_length();
+			uint64_t data64 = 0;
+			unsigned int bitoffset;
+			unsigned int pos = 0;
+
+			bitoffset = (trans.get_address() * 8) % DATA_WIDTH;
+
+			while (pos < len) {
+				rready.write(true);
+
+				wait(clk.posedge_event());
+
+				if (rvalid.read()) {
+					sc_bv<128> data128 = 0;
+					unsigned int readlen;
+					unsigned int w;
+
+					readlen = (DATA_WIDTH - bitoffset) / 8;
+					readlen = readlen <= len ? readlen : len;
+
+					for (w = 0; w < readlen; w += sizeof data64) {
+						int copylen = readlen <= sizeof data64 ? readlen : sizeof data64;
+						data128 = rdata.read() >> (w * 8 + bitoffset);
+						data64 = data128.to_uint64();
+						memcpy(data + pos, &data64, copylen);
+						D(printf("Read dr=%d data64=%lx pos=%d\n",
+							dr, data64, pos,
+							(w * sizeof data64 * 8 + bitoffset)));
+						pos += copylen;
+					}
+
+					bitoffset = 0;
+
+				}
+			}
+			rready.write(false);
+
+			// Set response
+			switch (rresp.read().to_uint64()) {
+			case AXI_OKAY:
+			case AXI_EXOKAY:
+				trans.set_response_status(tlm::TLM_OK_RESPONSE);
+				break;
+			case AXI_DECERR:
+				D(printf("DECERR\n"));
+				trans.set_response_status(tlm::TLM_ADDRESS_ERROR_RESPONSE);
+				break;
+			case AXI_SLVERR:
+				D(printf("SLVERR\n"));
+				trans.set_response_status(tlm::TLM_GENERIC_ERROR_RESPONSE);
+				break;
+			}
+
+			tr->DoneEvent().notify();
+		}
+	}
+
+	void write_data_phase()
+	{
+		wvalid.write(false);
+
+		while (true) {
+			Transaction *tr = wrDataFifo.read();
+			tlm::tlm_generic_payload& trans = tr->GetGP();
+			sc_time& delay = tr->GetDelay();
+			unsigned int len = trans.get_data_length();
+			unsigned int beat = 1;
+			unsigned int nr_beats = GetNumBeats(trans);
+			unsigned int pos = 0;
+
+			while (pos < len) {
+				pos += prepare_wbeat(trans, delay, pos);
+				wlast.write(beat == nr_beats);
+				beat++;
+				do {
+					wait(clk.posedge_event());
+				} while (wready.read() == false);
+			}
+
+			wlast.write(false);
+			wvalid.write(false);
+
+			wrRespFifo.write(tr);
+		}
+	}
+
+	void write_resp_phase()
+	{
+		bready.write(false);
+
+		while (true) {
+			Transaction *tr = wrRespFifo.read();
+			tlm::tlm_generic_payload& trans = tr->GetGP();
+
+			bready.write(true);
+			do {
+				wait(clk.posedge_event());
+			} while (bvalid.read() == false);
+			bready.write(false);
+
+			// Set TLM response
+			switch (bresp.read().to_uint64()) {
+			case AXI_OKAY:
+			case AXI_EXOKAY:
+				trans.set_response_status(tlm::TLM_OK_RESPONSE);
+				break;
+			case AXI_DECERR:
+				D(printf("DECERR\n"));
+				trans.set_response_status(tlm::TLM_ADDRESS_ERROR_RESPONSE);
+				break;
+			case AXI_SLVERR:
+				D(printf("SLVERR\n"));
+				trans.set_response_status(tlm::TLM_GENERIC_ERROR_RESPONSE);
+				break;
+			}
+
+			tr->DoneEvent().notify();
+		}
+	}
 };
 
 template
-<class BOOL_TYPE, template <int> class ADDR_TYPE, int ADDR_WIDTH, template <int> class DATA_TYPE, int DATA_WIDTH>
-tlm2axi_bridge<BOOL_TYPE, ADDR_TYPE, ADDR_WIDTH, DATA_TYPE, DATA_WIDTH> ::tlm2axi_bridge(sc_module_name name)
+<class BOOL_TYPE, template <int> class ADDR_TYPE, int ADDR_WIDTH, template <int> class DATA_TYPE, int DATA_WIDTH, int ID_WIDTH>
+tlm2axi_bridge<BOOL_TYPE, ADDR_TYPE, ADDR_WIDTH, DATA_TYPE, DATA_WIDTH, ID_WIDTH> ::tlm2axi_bridge(sc_module_name name)
 	: sc_module(name), tgt_socket("tgt-socket"),
 	clk("clk"),
 	awvalid("awvalid"),
@@ -136,12 +464,19 @@ tlm2axi_bridge<BOOL_TYPE, ADDR_TYPE, ADDR_WIDTH, DATA_TYPE, DATA_WIDTH> ::tlm2ax
 	rresp("rresp")
 {
 	tgt_socket.register_b_transport(this, &tlm2axi_bridge::b_transport);
+
+	SC_THREAD(address_phase);
+
+	SC_THREAD(read_resp_phase);
+
+	SC_THREAD(write_data_phase);
+	SC_THREAD(write_resp_phase);
 }
 
 template
-<class BOOL_TYPE, template <int> class ADDR_TYPE, int ADDR_WIDTH, template <int> class DATA_TYPE, int DATA_WIDTH>
+<class BOOL_TYPE, template <int> class ADDR_TYPE, int ADDR_WIDTH, template <int> class DATA_TYPE, int DATA_WIDTH, int ID_WIDTH>
 int tlm2axi_bridge
-<BOOL_TYPE, ADDR_TYPE, ADDR_WIDTH, DATA_TYPE, DATA_WIDTH>
+<BOOL_TYPE, ADDR_TYPE, ADDR_WIDTH, DATA_TYPE, DATA_WIDTH, ID_WIDTH>
 ::prepare_wbeat(tlm::tlm_generic_payload& trans, sc_time& delay,
                 unsigned int offset)
 {
@@ -197,166 +532,11 @@ int tlm2axi_bridge
 
 	wstrb.write(strb);
 	wvalid.write(true);
-	bready.write(true);
 	return wlen;
 }
 
-template
-<class BOOL_TYPE, template <int> class ADDR_TYPE, int ADDR_WIDTH, template <int> class DATA_TYPE, int DATA_WIDTH>
-void tlm2axi_bridge
-<BOOL_TYPE, ADDR_TYPE, ADDR_WIDTH, DATA_TYPE, DATA_WIDTH>
-::b_transport(tlm::tlm_generic_payload& trans, sc_time& delay)
-{
-	tlm::tlm_command cmd = trans.get_command();
-	sc_dt::uint64 addr = trans.get_address();
-	unsigned char *data = trans.get_data_ptr();
-	unsigned int len = trans.get_data_length();
-	genattr_extension *genattr;
-	unsigned int bitoffset;
-	unsigned int pos = 0;
-	uint64_t data64 = 0;
-	bool ar, dr, alldone = false;
-	unsigned int resp = AXI_DECERR;
-	unsigned int nr_beats = (len * 8 + DATA_WIDTH - 1) / DATA_WIDTH;
-	unsigned int beat = 0;
-	sc_bv<128> data128 = 0;
-	unsigned int prot = 0;
 
-	D(printf("TLM2AXI addr=%lx rw=%d len=%d nr-beats=%d\n",
-		addr, cmd == tlm::TLM_WRITE_COMMAND, len,
-		nr_beats));
+typedef tlm2axi_bridge<bool, sc_bv, 32, sc_bv, 32> TLM2AXI_bridge;
 
-	/* Extensions.  */
-	trans.get_extension(genattr);
-	if (genattr) {
-		if (genattr->get_non_secure()) {
-			prot |= AXI_PROT_NS;
-		}
-	}
-
-	/* Send the address.  */
-	if (cmd == tlm::TLM_READ_COMMAND) {
-		araddr.write(addr);
-		arprot.write(prot);
-		arsize.write(7);
-		arlen.write(nr_beats - 1);
-		arburst.write(AXI_BURST_INCR);
-
-		arvalid.write(true);
-	} else {
-		awaddr.write(addr & ~0xf);
-		awprot.write(prot);
-		awsize.write(7);
-		awlen.write(nr_beats - 1);
-		awburst.write(AXI_BURST_INCR);
-
-		awvalid.write(true);
-	}
-
-	/* Prepare first beat of data.  */
-	bitoffset = (addr * 8) % DATA_WIDTH;
-	if (cmd == tlm::TLM_READ_COMMAND) {
-		rready.write(true);
-	} else {
-		pos += prepare_wbeat(trans, delay, pos);
-		beat++;
-		alldone = pos == len;
-		wlast.write(alldone);
-	}
-
-	/* Wait for address and data phases to finish.  */
-	ar = false;
-	dr = false;
-	do {
-		wait(clk.posedge_event());
-
-		if (cmd == tlm::TLM_READ_COMMAND) {
-			ar |= arready.read();
-			dr = rvalid.read();
-			if (ar)
-				arvalid.write(false);
-
-			if (dr) {
-				unsigned int readlen;
-				unsigned int w;
-
-				readlen = (DATA_WIDTH - bitoffset) / 8;
-				readlen = readlen <= len ? readlen : len;
-
-				for (w = 0; w < readlen; w += sizeof data64) {
-					int copylen = readlen <= sizeof data64 ? readlen : sizeof data64;
-					data128 = rdata.read() >> (w * 8 + bitoffset);
-					data64 = data128.to_uint64();
-					memcpy(data + pos, &data64, copylen);
-					D(printf("Read dr=%d data64=%lx pos=%d\n",
-						dr, data64, pos,
-						(w * sizeof data64 * 8 + bitoffset)));
-					pos += copylen;
-				}
-				bitoffset = 0;
-
-				resp = rresp.read().to_uint64();
-				alldone = pos == len;
-			}
-		} else {
-			ar |= awready.read();
-			dr = wready.read();
-			if (ar)
-				awvalid.write(false);
-
-			if (dr) {
-				alldone = pos == len;
-				if (!alldone) {
-					pos += prepare_wbeat(trans, delay, pos);
-					wlast.write(alldone);
-					beat++;
-					wlast.write(beat == nr_beats);
-				} else {
-					D(printf("beat=%d nr-beats=%d\n", beat, nr_beats));
-					assert(beat == nr_beats);
-					wvalid.write(false);
-				}
-			}
-		}
-		D(printf("wr=%d pos=%d len=%d\n", cmd == tlm::TLM_WRITE_COMMAND,
-			pos, len));
-		fflush(NULL);
-	} while (!ar || !dr || !alldone);
-
-	arvalid.write(false);
-	rready.write(false);
-	awvalid.write(false);
-	wvalid.write(false);
-
-	if (cmd == tlm::TLM_WRITE_COMMAND) {
-		bool r;
-		/* Wait for Bresp channel.  */
-		do {
-			r = bvalid.read();
-			resp = bresp.read().to_uint64();
-			if (!r)
-				wait(clk.posedge_event());
-		} while (r == false);
-	}
-	bready.write(false);
-
-	D(printf("DONE resp=%d\n\n", resp));
-
-	switch (resp) {
-	case AXI_OKAY:
-	case AXI_EXOKAY:
-		trans.set_response_status(tlm::TLM_OK_RESPONSE);
-		break;
-	case AXI_DECERR:
-		D(printf("DECERR\n"));
-		trans.set_response_status(tlm::TLM_ADDRESS_ERROR_RESPONSE);
-		break;
-	case AXI_SLVERR:
-		D(printf("SLVERR\n"));
-		trans.set_response_status(tlm::TLM_GENERIC_ERROR_RESPONSE);
-		break;
-	}
-	D(fflush(NULL));
-}
 #undef D
 #endif
