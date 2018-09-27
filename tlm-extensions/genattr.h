e4b062e8c094e8c50c099dc3f25266d5fdf8460a
Francisco Iglesias <francisco.iglesias@xilinx.com>
2018-09-27 21:59:05 +0200

    genattr: Add exclusive, locked, QoS, region and cacheable properties
    Add properties to genattr_bus for supporting descriptions of exclusive
    transactions, locked transactions, transactions that are bufferable while
    progressing through the system, cacheable transactions and the QoS and region
    of the transactions.
    
    Signed-off-by: Francisco Iglesias <francisco.iglesias@xilinx.com>
    Signed-off-by: Edgar E. Iglesias <edgar.iglesias@xilinx.com>

--- tlm-extensions/genattr.h
+++ tlm-extensions/genattr.h
@@ -73,17 +73,73 @@ private:
 	uint32_t burst_width;
 
 	uint32_t transaction_id;
+
+	// For AMBA AXI this marks the transaction as exclusive.
+	bool exclusive;
+
+	// For AMBA AXI3 this marks the transaction as locked.
+	bool locked;
+
+	// AMBA AXI bufferable attribute. When true, an interconnect, or any
+	// component, can delay the transaction reaching its final destination
+	// for any number of cycles.
+	bool bufferable;
+
+	// AMBA AXI modifiable attribute. When true, modifiable indicates that
+	// the characteristics of the transaction can be modified. When
+	// modifiable is false, the transaction is Non-modifiable.
+	bool modifiable;
+
+	// For AMBA AXI3 this is read allocate.
+	// For AMBA AXI4 this is allocate for read transactions and other
+	// allocate for write transactions.
+	bool read_allocate;
+
+	// For AMBA AXI3 this is write allocate.
+	// For AMBA AXI4 this is allocate for write transactions and other
+	// allocate for read transactions.
+	bool write_allocate;
+
+	// AMBA AXI QoS identifier recommended to be used as a priority
+	// indicator where a higher value indicates a higher priority
+	// transaction.
+	uint8_t qos;
+
+	// AMBA AXI region identifier, with AMBA AXI it can be used to uniquely
+	// identify up to sixteen different regions. The region identifier can
+	// provide a decode of higher order address bits. Usage of region
+	// identifiers means a single physical interface on a slave can provide
+	// multiple logical interfaces, each with a different location in the
+	// system address map.
+	uint8_t region;
+
 public:
 	PROP_GETSET_GEN(secure, bool, true);
 	PROP_GETSET_GEN(master_id, uint64_t, 0);
 	PROP_GETSET_GEN(burst_width, uint32_t, 0);
 	PROP_GETSET_GEN(transaction_id, uint32_t, 0);
+	PROP_GETSET_GEN(exclusive, bool, false);
+	PROP_GETSET_GEN(locked, bool, false);
+	PROP_GETSET_GEN(bufferable, bool, false);
+	PROP_GETSET_GEN(modifiable, bool, false);
+	PROP_GETSET_GEN(read_allocate, bool, false);
+	PROP_GETSET_GEN(write_allocate, bool, false);
+	PROP_GETSET_GEN(qos, uint8_t, 0);
+	PROP_GETSET_GEN(region, uint8_t, 0);
 
 	genattr_bus() {
 		set_secure();
 		set_master_id();
 		set_burst_width();
 		set_transaction_id();
+		set_exclusive();
+		set_locked();
+		set_bufferable();
+		set_modifiable();
+		set_read_allocate();
+		set_write_allocate();
+		set_qos();
+		set_region();
 	}
 
 	// Compatibility layer for older versions and other frameworks.
@@ -108,6 +164,16 @@ public:
 		set_eop(ext_genattr.get_eop());
 		set_master_id(ext_genattr.get_master_id());
 		set_secure(ext_genattr.get_secure());
+		set_burst_width(ext_genattr.get_burst_width());
+		set_transaction_id(ext_genattr.get_transaction_id());
+		set_exclusive(ext_genattr.get_exclusive());
+		set_locked(ext_genattr.get_locked());
+		set_bufferable(ext_genattr.get_bufferable());
+		set_modifiable(ext_genattr.get_modifiable());
+		set_read_allocate(ext_genattr.get_read_allocate());
+		set_write_allocate(ext_genattr.get_write_allocate());
+		set_qos(ext_genattr.get_qos());
+		set_region(ext_genattr.get_region());
 	}
 
 	tlm::tlm_extension_base *clone(void) const
