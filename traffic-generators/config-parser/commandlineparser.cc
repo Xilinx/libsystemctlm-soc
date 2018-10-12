d590447ee925d9220af2a59b9949279628eef6c7
Vikram Vikram <fnuv@xilinx.com>
2018-10-12 22:24:15 +0200

    traffic-generators: Add support for traffic generator command line options
    Add support for parsing traffic generator related command line options.
    
    Signed-off-by: Vikram Vikram <fnuv@xilinx.com>

---
+++ traffic-generators/config-parser/commandlineparser.cc
@@ -0,0 +1,151 @@
+// Input parser CmdLineParser
+//
+// Parses a json format configuration file in order to serialize/deserialize
+// the DataTransfer object.
+//
+// Copyright 2018 (C) Xilinx Inc.
+// Written by Vikram<fnuv@xilinx.com>
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
+// THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+//
+#include <string>
+#include <iostream>
+#include <unistd.h>
+#include <vector>
+#include <regex>
+#include <thread>
+#include <vector>
+#include "commandlineparser.h"
+#include "data-transfer.h"
+#include "parserfacade.h"
+#include "deserializer.h"
+
+using namespace std;
+
+
+static void show_usage()
+{
+	std::cerr << "Usage: " << " <option(s)> config file names\n\t" <<"For example:- '-p -d config1.json config2.json'\n"
+		<< "Options:\n"
+		<< "\t-h,--help\tShow this help message\n"
+		<< "\t-d,--debug\tenables the debug. Will dump out error messages if any\n"
+		<< "\t-p,--parallel\tthis options does the parallel parsing"
+		<< std::endl;
+}
+
+// Global static pointer used to ensure a single instance of the class.
+//CmdLineParser* CmdLineParser::m_pinstance = NULL;
+
+static std::string getCmdOption(int argc, char* argv[], const std::string& option)
+{
+    std::string path_local;
+     for( int i = 0; i < argc; ++i)
+     {
+          std::string arg = argv[i];
+          if(0 == arg.find(option))
+          {
+               path_local =arg.substr(option.length());
+               return path_local;
+          }
+     }
+     return path_local;
+}
+
+void CmdLineParser::cmd_Parse(const int argc,  char ** const argv){
+	int opt;
+	string input = "";
+	opterr = 0;
+	// std::string::size_type n;
+	//check for option argument.
+	while ( (opt = getopt(argc, argv, "hpd")) != -1 ) {  // for each option...
+		switch ( opt ) {
+			case 'h':
+        //call the function show_usage.
+				show_usage();
+				break;
+			case 'p':
+				parallel_flag = true;
+				break;
+			case 'd':
+				enable_debug = true;
+				break;
+			case '?':  // unknown option...
+				cerr << "Unknown option: " << char(optopt) << "' Use '-h' for help" << endl;
+				break;
+		}
+	}
+	path = getCmdOption(argc, argv, "tp=");
+
+	// Retrieve the (non-option) argument:
+	if ( (argc <= 1) || (argv[argc-1] == NULL) || (argv[argc-1][0] == '-') ) {  // there is NO input...
+		cerr << "No inputs/config(json) file names provided. Use 'h' option for the help!" << endl;
+	}
+	else {  // there is an input, look if we have  been given a json file name.
+		for (int i = 1; i <argc; i++){
+			input = argv[i];
+			string arg1(argv[i]);
+			// An object of regex for pattern to be searched
+			regex r(".json");
+			// parallel_flag type for determining the matching behavior
+			// here it is for matches on 'string' objects
+			smatch m;
+			// regex_search() for searching the regex pattern
+			// 'r' in the string 's'. 'm' is parallel_flag for determining
+			// matching behavior.
+			if(regex_search(arg1, m, r)){
+				config_file_names.push_back(argv[i]);
+			}
+		}
+		for(uint i=0; i<config_file_names.size();i++){
+			string temp = path;
+			temp += config_file_names[i];
+			DataTransferVec dataTransferVector;
+	    	ParserFacade::Deserialize(dataTransferVector, temp.c_str());
+		}
+	}
+}
+//If user passes parameter. Come here and parse
+CmdLineParser& CmdLineParser::InstanceCmdLineParser(const int argc,  char ** const argv){
+		static CmdLineParser m_pinstance;
+
+		if((argv != 0) && (argv != nullptr)){
+				m_pinstance.cmd_Parse(argc, argv);
+		}
+
+		return(m_pinstance);
+};
+
+//Returns bool value of '-p' option.
+bool CmdLineParser::getParallel() const{
+	return(parallel_flag);
+}
+
+bool CmdLineParser::getDebugModeStatus() const{
+	return(enable_debug);
+}
+
+std::string CmdLineParser::getPath() const{
+	return(path);
+}
+
+CmdLineParser::~CmdLineParser(){
+}
+
+const vector<string> CmdLineParser::getConfigs() const{
+	return config_file_names;
+}
---
+++ traffic-generators/config-parser/commandlineparser.h
@@ -0,0 +1,68 @@
+// Input parser CmdLineParser
+//
+// Parses a json format configuration file in order to serialize/deserialize
+// the DataTransfer object.
+//
+// Copyright 2018 (C) Xilinx Inc.
+// Written by Vikram<fnuv@xilinx.com>
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
+// THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+//
+#ifndef COMMAND_LINE_PARSER_H__
+#define COMMAND_LINE_PARSER_H__
+#include <stdio.h>
+#include <vector>
+#include <string>
+using namespace std;
+//
+// CmdLineParser Class
+//
+class CmdLineParser{
+    public:
+        //constructor
+        static CmdLineParser& InstanceCmdLineParser(const int argc = 0,  char ** const argv = nullptr);
+        //static CmdLineParser* InstanceCmdLineParser();
+        //destrcutor
+        virtual ~CmdLineParser();
+        bool getParallel() const; //make them const
+        bool getDebugModeStatus() const;
+        const vector<string> getConfigs() const;
+        string getPath() const;
+      private:
+        bool parallel_flag;
+        bool enable_debug;
+        vector<string> config_file_names;
+        std::string path ;
+        void cmd_Parse(const int argc,  char ** const argv);
+
+        CmdLineParser(){
+          	parallel_flag = false;
+          	enable_debug = false;
+            vector <char*> config_file_names;
+		    path = " ";
+        };
+        // make copy constructor private
+        CmdLineParser(CmdLineParser&){};
+        //assignment operator is private too
+        CmdLineParser& operator = (CmdLineParser const&);
+        //static CmdLineParser* m_pinstance;
+        void sequential_Run(const char* const config_file, bool debug);
+
+};
+
+#endif //COMMAND_LINE_PARSER_H__
