ddbd83e2d1be3177f0cdeb7f65020ff89ed4440b
Edgar E. Iglesias <edgar.iglesias@xilinx.com>
2020-05-19 12:45:07 +0200

    remote-port: pci: Add a remote-port TLM PCI device
    Reviewed-by: Francisco Iglesias <francisco.iglesias@xilinx.com>
    Signed-off-by: Edgar E. Iglesias <edgar.iglesias@xilinx.com>

---
+++ libremote-port/remote-port-tlm-pci-ep.cc
@@ -0,0 +1,97 @@
+/*
+ * System-C TLM-2.0 remoteport PCIe device.
+ *
+ * Copyright (c) 2020 Xilinx Inc
+ * Written by Edgar E. Iglesias
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
+ * THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+
+#define SC_INCLUDE_DYNAMIC_PROCESSES
+
+#include "systemc.h"
+#include "tlm_utils/simple_initiator_socket.h"
+#include "tlm_utils/simple_target_socket.h"
+
+#include "remote-port-tlm-pci-ep.h"
+
+void remoteport_tlm_pci_ep::propagate_rst(void) {
+	sig_rst.write(rst.read());
+}
+
+/*
+ * RP-dev allocation.
+ *
+ * We allocate 20 RP devices for a single PCIe device.
+ * dev          Function
+ * 0            Config space
+ * 1            Legacy IRQ
+ * 2            Reserved for Messages
+ * 3            DMA from the End-point towards us.
+ * 4 - 9        Reserved
+ * 10 - 20      IO or Memory Mapped BARs (6 + 4 reserved)
+ */
+
+// Connect all remote-port objects to public members.
+void remoteport_tlm_pci_ep::connect_rp_devs(int rp_dev_base) {
+	int i;
+
+	if (rp_io.size() + rp_mmio.size() > 6) {
+		SC_REPORT_ERROR("rp-pci-ep", "Too many BARs!\n");
+		return;
+	}
+
+	// Reset propagation.
+	adaptor->rst(sig_rst);
+	SC_METHOD(propagate_rst);
+	dont_initialize();
+	sensitive << rst;
+
+	config = &rp_config.sk;
+	adaptor->register_dev(rp_dev_base, &rp_config);
+	adaptor->register_dev(rp_dev_base + 1, &rp_irq);
+	adaptor->register_dev(rp_dev_base + 3, &rp_dma);
+
+	for (i = 0; i < rp_io.size(); i++) {
+		bar[i] = &rp_io[i].sk;
+
+		adaptor->register_dev(rp_dev_base + 10 + i, &rp_io[i]);
+	}
+
+	for (i = 0; i < rp_mmio.size(); i++) {
+		bar[rp_io.size() + i] = &rp_mmio[i].sk;
+		adaptor->register_dev(rp_dev_base + 10 + rp_io.size() + i, &rp_mmio[i]);
+	}
+}
+
+void remoteport_tlm_pci_ep::bind(pci_device_base &dev) {
+	int i;
+
+	config->bind(dev.config);
+	dev.dma.bind(dma);
+
+	for (i = 0; i < dev.bar.size(); i++) {
+		bar[i]->bind(dev.bar[i]);
+	}
+
+	for (i = 0; i < dev.irq.size() && i < irq.size(); i++) {
+		irq[i](signals_irq[i]);
+		dev.irq[i](signals_irq[i]);
+	}
+}
---
+++ libremote-port/remote-port-tlm-pci-ep.h
@@ -0,0 +1,156 @@
+/*
+ * Remote-port PCIe device.
+ *
+ * In a QEMU co-simulation, this module allows SystemC to model a PCIe EP
+ * attached to a QEMU PCIe RC.
+ *
+ * Copyright (c) 2020 Xilinx Inc
+ * Written by Edgar E. Iglesias
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
+ * THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+#ifndef REMOTE_PORT_TLM_PCIE_H
+#define REMOTE_PORT_TLM_PCIE_H
+
+#include <assert.h>
+#include "remote-port-tlm.h"
+#include "remote-port-tlm-memory-master.h"
+#include "remote-port-tlm-memory-slave.h"
+#include "remote-port-tlm-wires.h"
+
+#include "soc/pci/core/pci-device-base.h"
+
+// Remote-Port PCIe End-Point
+class remoteport_tlm_pci_ep
+	: public sc_module
+{
+	SC_HAS_PROCESS(remoteport_tlm_pci_ep);
+private:
+	remoteport_tlm_memory_slave rp_dma;
+	remoteport_tlm_wires rp_irq;
+
+public:
+	sc_in<bool> rst;
+
+	// TLM Socket to serve PCIe Config Space acceses.
+	tlm_utils::simple_initiator_socket<remoteport_tlm_memory_master> *config;
+	// BARs (IO or MMIO).
+	tlm_utils::simple_initiator_socket<remoteport_tlm_memory_master> *bar[6];
+	tlm_utils::simple_target_socket<remoteport_tlm_memory_slave> &dma;
+
+	// Interrupts.
+	// For legacy interrupts, this will be a vector of size 1.
+	// When using MSI/MSI-X, you can either send transactions over the
+	// dma socket or create a larger irq vector and let the remote-port peer
+	// convert these signals into MSI/MSI-X messages.
+	sc_vector<sc_in<bool> > &irq;
+
+	sc_vector<sc_signal<bool> > signals_irq;
+
+	// Constructor that auto-creates a new Remote-port adaptor.
+	// This is suitable for having one Remote-port session per
+	// PCIe EP, e.g in a hotpluggable setup.
+	remoteport_tlm_pci_ep(sc_module_name name,
+			int nr_io_bars,
+			int nr_mmio_bars,
+			int nr_irqs,
+			const char *sk_descr,
+			Iremoteport_tlm_sync *sync = NULL,
+			bool blocking_socket = false)
+		: sc_module(name),
+		  rp_dma("rp-dma"),
+		  rp_irq("rp-irq", nr_irqs, 0),
+		  rst("rst"),
+		  dma(rp_dma.sk),
+		  irq(rp_irq.wires_in),
+		  signals_irq("signals-irq", nr_irqs),
+		  free_adaptor(true),
+		  rp_config("rp-config"),
+		  rp_io("rp-io", nr_io_bars),
+		  rp_mmio("rp-mmio", nr_mmio_bars),
+		  tieoff_config(NULL)
+	{
+		assert(sk_descr);
+		adaptor = new remoteport_tlm("rp", -1, sk_descr,
+					sync, blocking_socket);
+
+		connect_rp_devs(0);
+	}
+
+	// Constructor that uses a pre-existing Remote-port adaptor.
+	// This is suitable for setups where the PCIe connections are
+	// fixes and not run-time pluggable.
+	remoteport_tlm_pci_ep(sc_module_name name,
+			int nr_io_bars,
+			int nr_mmio_bars,
+			int nr_irqs,
+			remoteport_tlm *adaptor,
+			int rp_dev_base)
+		: sc_module(name),
+		  rp_dma("rp-dma"),
+		  rp_irq("rp-irq", nr_irqs, 0),
+		  rst("rst"),
+		  dma(rp_dma.sk),
+		  irq(rp_irq.wires_in),
+		  signals_irq("signals-irq", nr_irqs),
+		  adaptor(adaptor),
+		  free_adaptor(false),
+		  rp_config("rp-config"),
+		  rp_io("rp-io", nr_io_bars),
+		  rp_mmio("rp-mmio", nr_mmio_bars),
+		  tieoff_config(NULL)
+	{
+		assert(adaptor);
+		connect_rp_devs(rp_dev_base);
+	}
+
+	~remoteport_tlm_pci_ep() {
+		if (free_adaptor) {
+			delete adaptor;
+		}
+		if (tieoff_config) {
+			delete tieoff_config;
+		}
+	}
+
+	void bind(pci_device_base &pcidev);
+
+	void before_end_of_elaboration(void) {
+		if (!rp_config.sk.size()) {
+			tieoff_config =
+				new tlm_utils::simple_target_socket<remoteport_tlm_pci_ep>("tieoff-config");
+			rp_config.sk.bind(*tieoff_config);
+		}
+	}
+private:
+	remoteport_tlm *adaptor;
+	bool free_adaptor;
+
+	sc_signal<bool> sig_rst;
+
+	remoteport_tlm_memory_master rp_config;
+	sc_vector<remoteport_tlm_memory_master> rp_io;
+	sc_vector<remoteport_tlm_memory_master> rp_mmio;
+
+	tlm_utils::simple_target_socket<remoteport_tlm_pci_ep> *tieoff_config;
+
+	void propagate_rst(void);
+	void connect_rp_devs(int rp_dev_base);
+};
+#endif
