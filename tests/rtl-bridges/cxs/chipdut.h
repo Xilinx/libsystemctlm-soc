8217480d4b111d4d9d8e3e91d69048172dbed3c6
Francisco Iglesias <francisco.iglesias@xilinx.com>
2020-06-11 21:24:54 +0100

    tests: rtl-bridges: cxs: Add simulation test
    Add a randomized simulation test for the RTL CXS bridge.
    
    Signed-off-by: Francisco Iglesias <francisco.iglesias@xilinx.com>
    Reviewed-by: Edgar E. Iglesias <edgar.iglesias@xilinx.com>
    Message-Id: <20200611202503.1780-31-francisco.iglesias@xilinx.com>
    Signed-off-by: Edgar E. Iglesias <edgar.iglesias@xilinx.com>

---
+++ tests/rtl-bridges/cxs/chipdut.h
@@ -0,0 +1,438 @@
+/*
+ * Copyright (c) 2020 Xilinx Inc.
+ * Written by Francisco Iglesias
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
+ * THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+#ifndef __CHIPDUT__H
+#define __CHIPDUT__H
+
+#include "tlm-bridges/tlm2chi-bridge-rnf.h"
+#include "tlm-bridges/chi2tlm-bridge-rnf.h"
+#include "tlm-bridges/tlm2chi-bridge-sn.h"
+#include "tlm-bridges/chi2tlm-bridge-sn.h"
+
+#include "tlm-bridges/tlm2axilite-bridge.h"
+#include "rtl-bridges/pcie-host/cxs/tlm/tlm2cxs-hw-bridge.h"
+
+#include "traffic-generators/tg-tlm.h"
+#include "traffic-generators/random-traffic.h"
+
+#include "test-modules/signals-axilite.h"
+#include "test-modules/signals-rnf-chi.h"
+#include "test-modules/signals-sn-chi.h"
+#include "test-modules/signals-cxs.h"
+
+#include "tlm-modules/rnf-chi.h"
+#include "tlm-modules/iconnect-chi.h"
+#include "tlm-modules/sn-chi.h"
+
+#include "Vcxs_bridge_top.h"
+
+#define CACHE_SIZE (4 * CACHELINE_SZ)
+#define RAM_SIZE (32 * CACHELINE_SZ)
+
+typedef CHISignals<
+tlm2chi_bridge_rnf<>::TXREQ_FLIT_WIDTH,
+tlm2chi_bridge_rnf<>::TXRSP_FLIT_WIDTH,
+tlm2chi_bridge_rnf<>::TXDAT_FLIT_WIDTH,
+tlm2chi_bridge_rnf<>::RXRSP_FLIT_WIDTH,
+tlm2chi_bridge_rnf<>::RXDAT_FLIT_WIDTH,
+tlm2chi_bridge_rnf<>::RXSNP_FLIT_WIDTH
+> CHISignals_t;
+
+typedef CHISignals_SN<
+tlm2chi_bridge_sn<>::TXREQ_FLIT_WIDTH,
+tlm2chi_bridge_sn<>::TXDAT_FLIT_WIDTH,
+tlm2chi_bridge_sn<>::RXRSP_FLIT_WIDTH,
+tlm2chi_bridge_sn<>::RXDAT_FLIT_WIDTH
+> CHISignals_SN_t;
+
+template<int RN_ID, int HN_ID,
+		int FLIT_WIDTH = 256,
+		int CNTL_WIDTH = 14>
+SC_MODULE(ChipDut)
+{
+	enum { SN_ID = HN_ID - 10 };
+
+	typedef CXSSignals<FLIT_WIDTH, CNTL_WIDTH> CXSSignals_t;
+
+	//
+	// CXS_DATACHECK = 'Parity' uses one CNTL CHK bit
+	//
+	typedef CXSChkSignals<FLIT_WIDTH/8, 1> CXSChkSignals_t;
+
+	//
+	// RN-F <-> ICN signals
+	//
+	CHISignals_t signals_rnf;
+
+	//
+	// SlaveNode <-> ICN signals
+	//
+	CHISignals_SN_t signals_sn;
+
+	//
+	// AXI4Lite signals toward dut
+	//
+	AXILiteSignals<64, 32 > signals_al;
+
+	//
+	// Other signals
+	//
+	sc_signal<bool> irq_out;
+	sc_signal<bool>	irq_ack;
+
+	sc_signal<sc_bv<128> > h2c_intr_out;
+	sc_signal<sc_bv<256> > h2c_gpio_out;
+
+	sc_signal<sc_bv<64> > c2h_intr_in;
+	sc_signal<sc_bv<256> > c2h_gpio_in;
+
+	sc_signal<sc_bv<4> > usr_resetn;
+
+	//
+	// Checking signals
+	//
+	CXSChkSignals_t dummy_tx_chk;
+	CXSChkSignals_t dummy_rx_chk;
+
+	//
+	// RN-F
+	//
+	RequestNode_F<RN_ID,
+			CACHE_SIZE,
+			HN_ID> rnf;
+
+	//
+	// ICN
+	//
+	iconnect_chi<HN_ID,
+			SN_ID,
+			1,
+			1> icn;
+
+	//
+	// RN-F <-> ICN bridges
+	//
+	tlm2chi_bridge_rnf<> t2c_bridge;
+	chi2tlm_bridge_rnf<> c2t_bridge;
+
+	//
+	// SlaveNode <-> ICN bridges
+	//
+	tlm2chi_bridge_sn<> t2c_bridge_sn;
+	chi2tlm_bridge_sn<> c2t_bridge_sn;
+
+	//
+	// TLM2CXS HW bridge
+	//
+	tlm2cxs_hw_bridge<> cxs_hwb;
+	tlm2axilite_bridge<64, 32 > al_bridge;
+	Vcxs_bridge_top dut;
+
+	//
+	// SlaveNode with memory
+	//
+	SlaveNode_F<SN_ID> sn;
+	memory mem;
+
+	ChipDut(sc_module_name name, sc_clock& clk, sc_signal<bool>& resetn) :
+
+		sc_module(name),
+
+		//
+		// Signals
+		//
+		signals_rnf("chi-signals"),
+		signals_sn("chi-signals-sn"),
+
+		signals_al("signals-al"),
+
+		//
+		// Other signals
+		//
+		irq_out("irq_out"),
+		irq_ack("irq_ack"),
+
+		h2c_intr_out("h2c_intr_out"),
+		h2c_gpio_out("h2c_gpio_out"),
+
+		c2h_intr_in("c2h_intr_in"),
+		c2h_gpio_in("c2h_gpio_in"),
+
+		usr_resetn("usr_resetn"),
+
+		dummy_tx_chk("cxs-tx-chk-signals"),
+		dummy_rx_chk("cxs-rx-chk-signals"),
+
+		//
+		// TLM components
+		//
+		rnf("rnf"),
+		icn("icn"),
+
+		t2c_bridge("tlm2chi-bridge"),
+		c2t_bridge("chi2tlm-bridge"),
+
+		t2c_bridge_sn("tlm2chi-bridge-sn"),
+		c2t_bridge_sn("chi2tlm-bridge-sn"),
+
+		cxs_hwb("cxs_hwb"),
+		al_bridge("al-bridge"),
+		dut("dut"),
+
+		sn("sn"),
+		mem("mem", sc_time(10, SC_NS), RAM_SIZE)
+	{
+		//
+		// Setup rnf with the interconnect
+		//
+		connect(clk, resetn,
+			rnf, t2c_bridge,
+			signals_rnf,
+			c2t_bridge,
+			*icn.port_RN_F[0],
+			*icn.port_CCIX[0],
+			cxs_hwb);
+
+		rnf.EnableDebug();
+
+		//
+		// Randomize transactions
+		//
+		rnf.GetCache().RandomizeTransactions(true);
+
+		//
+		// Setup slave node to the interconnect and memory
+		//
+		connect_sn(clk, resetn,
+			*icn.port_SN, t2c_bridge_sn,
+			signals_sn,
+			c2t_bridge_sn, sn, mem);
+
+		//
+		// Connect CXS HWB <-> tlm2axilite bridge
+		//
+		cxs_hwb.bridge_socket(al_bridge.tgt_socket);
+
+		//
+		// CXS HWB irq
+		//
+		cxs_hwb.irq(irq_out);
+
+		//
+		// Connect the AXI4Lite signals
+		//
+		signals_al.connect(al_bridge);
+		connect_al_signals(signals_al, dut);
+
+		//
+		// tlm2axilite bridge clk, resetn
+		//
+		al_bridge.clk(clk);
+		al_bridge.resetn(resetn);
+
+		//
+		// Other signals
+		//
+		dut.clk(clk);
+		dut.resetn(resetn);
+		dut.c2h_gpio_in(c2h_gpio_in);
+		dut.h2c_intr_out(h2c_intr_out);
+		dut.h2c_gpio_out(h2c_gpio_out);
+		dut.c2h_intr_in(c2h_intr_in);
+		dut.irq_out(irq_out);
+		dut.irq_ack(irq_ack);
+		dut.usr_resetn(usr_resetn);
+		usr_resetn.write(true);
+
+		dut.CXS_CRDRTN_CHK_TX(dummy_tx_chk.crdrtn_chk);
+		dut.CXS_CNTL_CHK_TX(dummy_tx_chk.cntl_chk);
+		dut.CXS_VALID_CHK_TX(dummy_tx_chk.valid_chk);
+		dut.CXS_CRDGNT_CHK_TX(dummy_tx_chk.crdgnt_chk);
+		dut.CXS_DATA_CHK_TX(dummy_tx_chk.data_chk);
+
+		dut.CXS_CRDRTN_CHK_RX(dummy_rx_chk.crdrtn_chk);
+		dut.CXS_CNTL_CHK_RX(dummy_rx_chk.cntl_chk);
+		dut.CXS_VALID_CHK_RX(dummy_rx_chk.valid_chk);
+		dut.CXS_CRDGNT_CHK_RX(dummy_rx_chk.crdgnt_chk);
+		dut.CXS_DATA_CHK_RX(dummy_rx_chk.data_chk);
+
+		irq_ack.write(true);
+	}
+
+	template<typename T1, typename T2,
+			typename T3, typename T4,
+			typename T5,
+			typename T6, typename T7,
+			typename T8, typename T9>
+	void connect(T1& clk, T2& resetn,
+			T3& rn, T4& tlm2chi_b,
+			T5& signals,
+			T6& chi2tlm_b,
+			T7& port_RN_F,
+			T8& port_CCIX,
+			T9& cxs_bridge)
+	{
+		// Connect clk
+		tlm2chi_b.clk(clk);
+		chi2tlm_b.clk(clk);
+
+		// Connect reset
+		tlm2chi_b.resetn(resetn);
+		chi2tlm_b.resetn(resetn);
+		cxs_bridge.resetn(resetn);
+
+		// Connect RN-F signals
+		signals.connectRNF(&tlm2chi_b);
+
+		// Connect ICN signals
+		signals.connectICN(&chi2tlm_b);
+
+		// Connect tlm2chi bridge on the RN
+		rn.connect(tlm2chi_b);
+
+		// Connect chi2tlm bridge to the interconnect port
+		port_RN_F.connect(chi2tlm_b);
+
+		//
+		// Connect ICN CCIX port to the TLM2CXS bridge
+		//
+		port_CCIX.txlink_init_socket(cxs_bridge.txlink_tgt_socket);
+		cxs_bridge.rxlink_init_socket(port_CCIX.rxlink_tgt_socket);
+
+		// Setup Port Node ID
+		port_RN_F.SetNodeID(RN_ID);
+	}
+
+	template<typename T1, typename T2,
+			typename T3, typename T4,
+			typename T5,
+			typename T6, typename T7, typename T8>
+	void connect_sn(T1& clk, T2& resetn,
+			T3& port_SN, T4& tlm2chi_b,
+			T5& signals,
+			T6& chi2tlm_b, T7& sn, T8& mem)
+	{
+		// Connect clk
+		tlm2chi_b.clk(clk);
+		chi2tlm_b.clk(clk);
+
+		// Connect reset
+		tlm2chi_b.resetn(resetn);
+		chi2tlm_b.resetn(resetn);
+
+		// Connect ICN signals
+		signals.connectICN(&tlm2chi_b);
+
+		// Connect SN signals
+		signals.connectSN(&chi2tlm_b);
+
+		// Connect tlm2ace bridge on the master
+		port_SN.connect(tlm2chi_b);
+
+		// Connect chi2tlm bridge to the interconnect port
+		sn.connect(chi2tlm_b);
+
+		// Connect the slave node to the memory
+		sn.init_socket(mem.socket);
+	}
+
+	template<typename T1, typename T2>
+	void connect_al_signals(T1& signals, T2& dut)
+	{
+		//
+		// Since the AXILite Dut doesn't use the same naming
+		// conventions as AXILiteSignals, we need to manually connect
+		// everything.
+		//
+		dut.s_axi_awvalid(signals.awvalid);
+		dut.s_axi_awready(signals.awready);
+		dut.s_axi_awaddr(signals.awaddr);
+		dut.s_axi_awprot(signals.awprot);
+
+		dut.s_axi_arvalid(signals.arvalid);
+		dut.s_axi_arready(signals.arready);
+		dut.s_axi_araddr(signals.araddr);
+		dut.s_axi_arprot(signals.arprot);
+
+		dut.s_axi_wvalid(signals.wvalid);
+		dut.s_axi_wready(signals.wready);
+		dut.s_axi_wdata(signals.wdata);
+		dut.s_axi_wstrb(signals.wstrb);
+
+		dut.s_axi_bvalid(signals.bvalid);
+		dut.s_axi_bready(signals.bready);
+		dut.s_axi_bresp(signals.bresp);
+
+		dut.s_axi_rvalid(signals.rvalid);
+		dut.s_axi_rready(signals.rready);
+		dut.s_axi_rdata(signals.rdata);
+		dut.s_axi_rresp(signals.rresp);
+	}
+
+	void ConnectRX(CXSSignals_t& sgnls)
+	{
+		/* Write address channel.  */
+		dut.CXS_ACTIVE_REQ_RX(sgnls.activereq);
+		dut.CXS_ACTIVE_ACK_RX(sgnls.activeack);
+		dut.CXS_DEACT_HINT_RX(sgnls.deacthint);
+
+		dut.CXS_VALID_RX(sgnls.valid);
+		dut.CXS_DATA_RX(sgnls.data);
+		dut.CXS_CNTL_RX(sgnls.cntl);
+		dut.CXS_CRDGNT_RX(sgnls.crdgnt);
+		dut.CXS_CRDRTN_RX(sgnls.crdrtn);
+	}
+
+	void ConnectTX(CXSSignals_t& sgnls)
+	{
+		/* Write address channel.  */
+		dut.CXS_ACTIVE_REQ_TX(sgnls.activereq);
+		dut.CXS_ACTIVE_ACK_TX(sgnls.activeack);
+		dut.CXS_DEACT_HINT_TX(sgnls.deacthint);
+
+		dut.CXS_VALID_TX(sgnls.valid);
+		dut.CXS_DATA_TX(sgnls.data);
+		dut.CXS_CNTL_TX(sgnls.cntl);
+		dut.CXS_CRDGNT_TX(sgnls.crdgnt);
+		dut.CXS_CRDRTN_TX(sgnls.crdrtn);
+	}
+
+	void Trace(sc_trace_file *trace_fp)
+	{
+		//
+		// Trace CHI RN-F <-> ICN signals
+		//
+		signals_rnf.Trace(trace_fp);
+
+		//
+		// Trace CHI SN <-> ICN signals
+		//
+		signals_sn.Trace(trace_fp);
+
+		//
+		// Trace AXI4Lite signals
+		//
+		signals_al.Trace(trace_fp);
+	}
+};
+
+#endif
---
+++ tests/rtl-bridges/cxs/cxs-test.cc
@@ -0,0 +1,289 @@
+/*
+ * Copyright (c) 2020 Xilinx Inc.
+ * Written by Francisco Iglesias
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
+ * THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+
+#define SC_INCLUDE_DYNAMIC_PROCESSES
+
+#include "systemc"
+using namespace sc_core;
+using namespace sc_dt;
+using namespace std;
+
+#include "tlm.h"
+#include "tlm_utils/simple_initiator_socket.h"
+#include "tlm_utils/simple_target_socket.h"
+
+#include "traffic-generators/tg-tlm.h"
+
+#include "test-modules/memory.h"
+#include "test-modules/chip-ccix.h"
+#include "test-modules/signals-cxs.h"
+#include "test-modules/utils-chi.h"
+#include "chipdut.h"
+
+#include <verilated_vcd_sc.h>
+#include "verilated.h"
+
+using namespace utils::CHI;
+
+#define LINE(l) ((l) * CACHELINE_SZ)
+
+#define RN0_ID 0
+#define HN0_ID 20
+
+#define RN1_ID 1
+#define HN1_ID 21
+
+// Increase for more random traffic
+#define NUM_TXNS_RNF0 20
+#define NUM_TXNS_RNF1 20000
+
+#define DATA_WIDTH 256
+#define CNTL_WIDTH 14
+
+typedef CCIX::LinkState LS;
+
+typedef CXSSignals<DATA_WIDTH, CNTL_WIDTH> CXSSignals_t;
+
+typedef Chip<RN0_ID, HN0_ID> Chip0_t;
+typedef ChipDut<RN1_ID, HN1_ID> ChipDut_t;
+
+typedef TLMTrafficGenerator::DoneCallback TGDoneCallBack;
+
+const unsigned char burst_data[140] = {
+	0x11, 0x12, 0x13, 0x14, 0x21, 0x22, 0x23, 0x24,
+	0x21, 0x22, 0x23, 0x24, 0x31, 0x32, 0x33, 0x34,
+	0x31, 0x32, 0x33, 0x34, 0x41, 0x42, 0x43, 0x44,
+	0x41, 0x42, 0x43, 0x44, 0x51, 0x52, 0x53, 0x54,
+
+	0x11, 0x12, 0x13, 0x14, 0x21, 0x22, 0x23, 0x24,
+	0x21, 0x22, 0x23, 0x24, 0x31, 0x32, 0x33, 0x34,
+	0x31, 0x32, 0x33, 0x34, 0x41, 0x42, 0x43, 0x44,
+	0x41, 0x42, 0x43, 0x44, 0x51, 0x52, 0x53, 0x54
+};
+
+sc_event *tgDone;
+
+void tgDoneCallBack(TLMTrafficGenerator *gen, int threadID)
+{
+	tgDone->notify();
+}
+
+TrafficDesc xfers(merge({
+	//
+	// ReadUnique
+	//
+	Write(LINE(0), DATA(0xFF, 0xFF, 0xFF, 0xFF)),
+	Read(LINE(0)),
+		Expect(DATA(0xFF, 0xFF, 0xFF, 0xFF), 4),
+
+	//
+	// WriteUniquePtl
+	//
+	Write(LINE(4), burst_data, CACHELINE_SZ),
+
+	//
+	// WriteCleanFull + WriteEvictFull + ReadOnce
+	//
+	Read(LINE(4), CACHELINE_SZ),
+		Expect(burst_data, CACHELINE_SZ),
+
+	//
+	// ReadOnce
+	//
+	Read(LINE(2)),
+		Expect(DATA(0x0, 0x0, 0x0, 0x0), 4),
+
+	//
+	// ReadSharedNotDirty + WriteEvictFull + MakeUnique
+	//
+	Read(LINE(1)),
+	Write(LINE(5), burst_data, CACHELINE_SZ),
+
+	//
+	// WriteBackFull + ReadOnceMakeInvalid + WriteUniquePtl
+	//
+	Read(LINE(17)),
+	Write(LINE(17), burst_data, CACHELINE_SZ),
+}));
+
+SC_MODULE(Top)
+{
+	sc_signal<bool> rst_n;
+	sc_clock clk;
+
+	CXSSignals_t sgnls_cxs0;
+	CXSSignals_t sgnls_cxs1;
+
+	RandomTraffic rand_traffic0;
+	RandomTraffic rand_traffic1;
+
+	Chip0_t chip0;
+	ChipDut_t chipDut; // Contains the RTL CXS bridge
+
+	SC_HAS_PROCESS(Top);
+
+	Top(sc_module_name name,
+		TrafficDesc& xfers0,
+		TGDoneCallBack tgDoneCB) :
+		sc_module(name),
+
+		rst_n("resetn", true),
+		clk("clk", sc_time(1, SC_US)),
+
+		sgnls_cxs0("cxs-signals0"),
+		sgnls_cxs1("cxs-signals1"),
+
+		rand_traffic0(0, RAM_SIZE, (~(0x3llu)),
+				1, RAM_SIZE, RAM_SIZE, NUM_TXNS_RNF0),
+		rand_traffic1(0, RAM_SIZE, (~(0x3llu)),
+				1, RAM_SIZE, RAM_SIZE, NUM_TXNS_RNF1),
+
+		chip0("chip0", xfers0, clk, rst_n),
+		chipDut("chip-dut", clk, rst_n),
+
+		m_tgDoneCB(tgDoneCB)
+	{
+		sc_time startDelay = sc_time(500, SC_US);
+
+		//
+		// Configure the System Address Map on HN0
+		//
+		chip0.icn.SystemAddressMap().AddMap(0, RAM_SIZE/2, HN1_ID);
+
+		//
+		// Configure the CCIX port on chip 0
+		//
+		chip0.icn.port_CCIX[0]->AddRemoteAgent(RN1_ID);
+		chip0.icn.port_CCIX[0]->AddRemoteAgent(HN1_ID);
+
+		chip0.EnableDebug();
+
+		//
+		// Configure the System Address Map on HN1
+		//
+		chipDut.icn.SystemAddressMap().AddMap(RAM_SIZE/2, RAM_SIZE/2, HN0_ID);
+
+		//
+		// Configure the CCIX port on chipDut
+		//
+		chipDut.icn.port_CCIX[0]->AddRemoteAgent(RN0_ID);
+		chipDut.icn.port_CCIX[0]->AddRemoteAgent(HN0_ID);
+
+		//
+		// Connect TX and RX Chip0
+		//
+		chip0.ConnectTX(sgnls_cxs0);
+		chip0.ConnectRX(sgnls_cxs1);
+
+		//
+		// Connect TX and RX chipDut
+		//
+		chipDut.ConnectRX(sgnls_cxs0);
+		chipDut.ConnectTX(sgnls_cxs1);
+
+		//
+		// Traffig generators
+		//
+		SetupTG(chip0, startDelay, xfers0, tgDoneCB);
+
+		SC_THREAD(tgDone_thread);
+	}
+
+	void tgDone_thread()
+	{
+		while (true) {
+			wait(tgDone);
+
+			if (!rand_traffic0.done()) {
+				SetupTG(chip0, SC_ZERO_TIME,
+					rand_traffic0, m_tgDoneCB);
+
+				SetupTG(chipDut, SC_ZERO_TIME, rand_traffic1);
+
+			} else {
+				sc_stop();
+			}
+		}
+	}
+
+	template<typename T1, typename T2>
+	void SetupTG(T1& chip, sc_time startDelay,
+			T2& xfers, TGDoneCallBack tgDoneCB = NULL)
+	{
+		TLMTrafficGenerator& tg = chip.rnf.GetTrafficGenerator();
+
+		tg.setStartDelay(startDelay);
+		tg.addTransfers(xfers, 0, tgDoneCB);
+	}
+
+	TGDoneCallBack m_tgDoneCB;
+	sc_event tgDone;
+};
+
+int sc_main(int argc, char *argv[])
+{
+	Verilated::commandArgs(argc, argv);
+	Top top("Top", xfers, tgDoneCallBack);
+
+	sc_trace_file *trace_fp = sc_create_vcd_trace_file(argv[0]);
+
+	sc_trace(trace_fp, top.clk, top.clk.name());
+	sc_trace(trace_fp, top.rst_n, top.rst_n.name());
+
+	sc_trace(trace_fp, top.chipDut.irq_out, top.chipDut.irq_out.name());
+	sc_trace(trace_fp, top.chipDut.irq_ack, top.chipDut.irq_ack.name());
+
+	top.sgnls_cxs0.Trace(trace_fp);
+	top.sgnls_cxs1.Trace(trace_fp);
+
+	top.chipDut.signals_al.Trace(trace_fp);
+
+#if VM_TRACE
+        Verilated::traceEverOn(true);
+        // If verilator was invoked with --trace argument,
+        // and if at run time passed the +trace argument, turn on tracing
+        VerilatedVcdSc* tfp = NULL;
+        const char* flag = Verilated::commandArgsPlusMatch("trace");
+        if (flag && 0 == strcmp(flag, "+trace")) {
+                tfp = new VerilatedVcdSc;
+                top.chipDut.dut.trace(tfp, 99);
+                tfp->open("vlt_dump.vcd");
+        }
+#endif
+	tgDone = &top.tgDone;
+
+	// Reset is active low. Emit a reset cycle.
+	top.rst_n.write(false);
+	sc_start(4, SC_US);
+	top.rst_n.write(true);
+
+	sc_start();
+
+	if (trace_fp) {
+		sc_close_vcd_trace_file(trace_fp);
+	}
+#if VM_TRACE
+        if (tfp) { tfp->close(); tfp = NULL; }
+#endif
+	return 0;
+}
