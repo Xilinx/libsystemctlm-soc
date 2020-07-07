784c4c204c8bba1d52b13cc142162995e10d749e
Edgar E. Iglesias <edgar.iglesias@xilinx.com>
2020-07-07 11:45:57 +0200

    utils: bindump: Add utility functions to print in binary
    Signed-off-by: Edgar E. Iglesias <edgar.iglesias@xilinx.com>

---
+++ utils/bindump.h
@@ -0,0 +1,45 @@
+/*
+ * Binary dump helper functions.
+ *
+ * Copyright (c) 2020 Xilinx Inc.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
+ * THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+
+#ifndef BINDUMP_H_
+#define BINDUMP_H_
+#include <stdio.h>
+#include <stdlib.h>
+
+static void print_binary(const char *s, uint32_t v) {
+	unsigned int i;
+
+	printf("%s:\t", s);
+	for (i = 0; i < (sizeof v) * 8; i++) {
+		bool bit;
+
+		bit = v & (1U << ((sizeof v * 8) - i - 1));
+		putchar(bit ? '1' : '0');
+		if (i && ((i + 1) % 8) == 0) {
+			putchar('.');
+		}
+	}
+	putchar('\n');
+}
+#endif
