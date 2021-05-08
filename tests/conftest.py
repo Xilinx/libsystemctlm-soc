dd79ea377824c3c9157a4814eacc144b715d2066
Edgar E. Iglesias <edgar.iglesias@xilinx.com>
2021-05-08 08:30:50 +0200

    tests: conftest: Add marker descriptions
    Signed-off-by: Edgar E. Iglesias <edgar.iglesias@xilinx.com>

--- tests/conftest.py
+++ tests/conftest.py
@@ -28,3 +28,14 @@ def pytest_runtest_makereport(item, call):
 	except:
 		pass
 
+def pytest_configure(config):
+	# Register additional markers
+	config.addinivalue_line("markers" ,"axi4: AMBA AXI 4")
+	config.addinivalue_line("markers" ,"bridge: A protocol bridge")
+	config.addinivalue_line("markers" ,"ccix: CCIX")
+	config.addinivalue_line("markers" ,"checker: A protocol checker")
+	config.addinivalue_line("markers" ,"examples: Example setups")
+	config.addinivalue_line("markers" ,"hw_bridge: A HW bridge / transactor")
+	config.addinivalue_line("markers" ,"pcie: PCI-express")
+	config.addinivalue_line("markers" ,"tg: A traffic generator")
+
