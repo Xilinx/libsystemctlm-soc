5e908119ee07af64967971979c04401626d5ec13
Francisco Iglesias <francisco.iglesias@xilinx.com>
2022-03-23 17:11:48 +0000

    soc: xilinx: Add an initial Xilinx Versal-Net SystemC wrapper
    Add an initial Versal-Net SystemC wrapper. The wrapper enables
    co-simulation with Xilinx QEMU Versal-Net models.
    
    Signed-off-by: Francisco Iglesias <francisco.iglesias@xilinx.com>
    Reviewed-by: Edgar E. Iglesias <edgar.iglesias@xilinx.com>
    Signed-off-by: Edgar E. Iglesias <edgar.iglesias@xilinx.com>

---
+++ soc/xilinx/versal-net/xilinx-versal-net.cc
@@ -0,0 +1,190 @@
+/*
+ * Xilinx SystemC/TLM-2.0 Versal Net Wrapper.
+ *
+ * Written by Francisco Iglesias <francisco.iglesias@amd.com>
+ *
+ * Copyright (c) 2022, Xilinx Inc.
+ * All rights reserved.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
+ * THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+
+#define SC_INCLUDE_DYNAMIC_PROCESSES
+
+#include <inttypes.h>
+
+#include "tlm_utils/simple_initiator_socket.h"
+#include "tlm_utils/simple_target_socket.h"
+
+using namespace sc_core;
+using namespace std;
+
+#include "xilinx-versal-net.h"
+#include <sys/types.h>
+
+#define VERSAL_NET_NUM_PL2PS_IRQ 16
+
+#define VERSAL_NET_NUM_PL_RESET 4
+#define VERSAL_NET_NUM_PS2PL_WIRES VERSAL_NET_NUM_PL_RESET
+
+xilinx_versal_net::xilinx_versal_net(sc_module_name name, const char *sk_descr,
+				Iremoteport_tlm_sync *sync,
+				bool blocking_socket)
+	: remoteport_tlm(name, -1, sk_descr, sync, blocking_socket),
+	  rp_m_fpd_axi_pl("rp_m_fpd_axi_pl"),
+	  rp_m_lpd_axi_pl("rp_m_lpd_axi_pl"),
+	  rp_m_fpd_axi_noc0("rp_m_fpd_axi_noc0"),
+	  rp_m_fpd_axi_noc1("rp_m_fpd_axi_noc1"),
+	  rp_m_fpd_axi_noc2("rp_m_fpd_axi_noc2"),
+	  rp_m_fpd_axi_noc3("rp_m_fpd_axi_noc3"),
+	  rp_m_fpd_axi_noc4("rp_m_fpd_axi_noc4"),
+	  rp_m_fpd_axi_noc5("rp_m_fpd_axi_noc5"),
+	  rp_m_fpd_axi_noc6("rp_m_fpd_axi_noc6"),
+	  rp_m_fpd_axi_noc7("rp_m_fpd_axi_noc7"),
+	  rp_m_fpd_axi_noc_iso("rp_m_fpd_axi_noc_iso"),
+	  rp_m_cpm_axi_noc0("rp_m_cpm_axi_noc0"),
+	  rp_m_cpm_axi_noc1("rp_m_cpm_axi_noc1"),
+	  rp_m_cpm_axi_noc2("rp_m_cpm_axi_noc2"),
+	  rp_m_cpm_axi_noc3("rp_m_cpm_axi_noc3"),
+	  rp_m_lpd_axi_noc0("rp_m_lpd_axi_noc0"),
+	  rp_reserved_0("rp_reserved_0"),
+	  rp_m_pmcx_axi_noc0("rp_m_pmcx_axi_noc0"),
+	  rp_pmc_npi("rp_pmc_npi"),
+	  rp_m_cpm("rp_m_cpm"),
+	  rp_m_hnic("rp_m_hnic"),
+
+	  rp_s_pl_chi_fpd("rp_s_pl_chi_fpd"),
+	  rp_s_pl_axi_fpd0("rp_s_pl_axi_fpd0"),
+	  rp_s_pl_axi_fpd1("rp_s_pl_axi_fpd1"),
+	  rp_s_pl_axi_fpd2("rp_s_pl_axi_fpd2"),
+	  rp_s_pl_axi_fpd3("rp_s_pl_axi_fpd3"),
+	  rp_s_noc_axi_fpd0("rp_s_noc_axi_fpd0"),
+	  rp_s_noc_axi_fpd1("rp_s_noc_axi_fpd1"),
+	  rp_s_noc_axi_fpd2("rp_s_noc_axi_fpd2"),
+	  rp_s_noc_axi_fpd3("rp_s_noc_axi_fpd3"),
+	  rp_s_pl_acp_apu("rp_s_pl_acp_apu"),
+	  rp_s_cpm("rp_s_cpm"),
+	  rp_s_noc_axi_cpm("rp_s_noc_axi_cpm"),
+	  rp_s_pl_axi_lpd("rp_s_pl_axi_lpd"),
+	  rp_s_noc_axi_pmcx0("rp_s_noc_axi_pmcx0"),
+
+	  rp_pl2ps_irq("rp_pl2ps_irq", VERSAL_NET_NUM_PL2PS_IRQ, 0),
+	  rp_wires_out("rp_wires_out", 0, VERSAL_NET_NUM_PS2PL_WIRES),
+	  pl2ps_irq("pl2ps_irq", VERSAL_NET_NUM_PL2PS_IRQ),
+	  pl_reset("pl_reset", VERSAL_NET_NUM_PL_RESET)
+{
+	unsigned int i;
+
+	m_fpd_axi_pl = &rp_m_fpd_axi_pl.sk;
+	m_lpd_axi_pl = &rp_m_lpd_axi_pl.sk;
+	m_fpd_axi_noc0 = &rp_m_fpd_axi_noc0.sk;
+	m_fpd_axi_noc1 = &rp_m_fpd_axi_noc1.sk;
+	m_fpd_axi_noc2 = &rp_m_fpd_axi_noc2.sk;
+	m_fpd_axi_noc3 = &rp_m_fpd_axi_noc3.sk;
+	m_fpd_axi_noc4 = &rp_m_fpd_axi_noc4.sk;
+	m_fpd_axi_noc5 = &rp_m_fpd_axi_noc5.sk;
+	m_fpd_axi_noc6 = &rp_m_fpd_axi_noc6.sk;
+	m_fpd_axi_noc7 = &rp_m_fpd_axi_noc7.sk;
+	m_fpd_axi_noc_iso = &rp_m_fpd_axi_noc_iso.sk;
+	m_cpm_axi_noc0 = &rp_m_cpm_axi_noc0.sk;
+	m_cpm_axi_noc1 = &rp_m_cpm_axi_noc1.sk;
+	m_cpm_axi_noc2 = &rp_m_cpm_axi_noc2.sk;
+	m_cpm_axi_noc3 = &rp_m_cpm_axi_noc3.sk;
+	m_lpd_axi_noc0 = &rp_m_lpd_axi_noc0.sk;
+	s_reserved_0 = &rp_reserved_0.sk;
+	m_pmcx_axi_noc0 = &rp_m_pmcx_axi_noc0.sk;
+	pmc_npi = &rp_pmc_npi.sk;
+	m_cpm = &rp_m_cpm.sk;
+	m_hnic = &rp_m_hnic.sk;
+
+	s_pl_chi_fpd = &rp_s_pl_chi_fpd.sk;
+	s_pl_axi_fpd0 = &rp_s_pl_axi_fpd0.sk;
+	s_pl_axi_fpd1 = &rp_s_pl_axi_fpd1.sk;
+	s_pl_axi_fpd2 = &rp_s_pl_axi_fpd2.sk;
+	s_pl_axi_fpd3 = &rp_s_pl_axi_fpd3.sk;
+	s_noc_axi_fpd0 = &rp_s_noc_axi_fpd0.sk;
+	s_noc_axi_fpd1 = &rp_s_noc_axi_fpd1.sk;
+	s_noc_axi_fpd2 = &rp_s_noc_axi_fpd2.sk;
+	s_noc_axi_fpd3 = &rp_s_noc_axi_fpd3.sk;
+	s_pl_acp_apu = &rp_s_pl_acp_apu.sk;
+	s_cpm = &rp_s_cpm.sk;
+	s_noc_axi_cpm = &rp_s_noc_axi_cpm.sk;
+	s_pl_axi_lpd = &rp_s_pl_axi_lpd.sk;
+	s_noc_axi_pmcx0 = &rp_s_noc_axi_pmcx0.sk;
+
+	for (i = 0; i < pl2ps_irq.size(); i++) {
+		rp_pl2ps_irq.wires_in[i](pl2ps_irq[i]);
+	}
+	for (i = 0; i < pl_reset.size(); i++) {
+		rp_wires_out.wires_out[i](pl_reset[i]);
+	}
+
+	register_dev(2, &rp_reserved_0);
+
+	register_dev(10, &rp_s_pl_axi_fpd0);
+	register_dev(12, &rp_s_pl_axi_fpd1);
+	register_dev(14, &rp_s_pl_axi_fpd2);
+
+	register_dev(15, &rp_s_pl_acp_apu);
+	register_dev(16, &rp_s_pl_chi_fpd);
+
+	register_dev(17, &rp_s_noc_axi_fpd0);
+	register_dev(18, &rp_s_noc_axi_fpd1);
+	register_dev(19, &rp_s_noc_axi_fpd2);
+	register_dev(20, &rp_s_noc_axi_fpd3);
+
+	register_dev(21, &rp_s_noc_axi_cpm);
+
+	register_dev(23, &rp_s_noc_axi_pmcx0);
+	register_dev(24, &rp_s_pl_axi_lpd);
+
+	register_dev(40, &rp_m_fpd_axi_pl);
+	register_dev(42, &rp_m_lpd_axi_pl);
+
+	register_dev(50, &rp_m_fpd_axi_noc0);
+	register_dev(51, &rp_m_fpd_axi_noc1);
+	register_dev(52, &rp_m_fpd_axi_noc2);
+	register_dev(53, &rp_m_fpd_axi_noc3);
+	register_dev(54, &rp_m_fpd_axi_noc4);
+	register_dev(55, &rp_m_fpd_axi_noc5);
+
+	register_dev(56, &rp_m_cpm_axi_noc0);
+	register_dev(57, &rp_m_cpm_axi_noc1);
+
+	register_dev(58, &rp_m_lpd_axi_noc0);
+	register_dev(59, &rp_m_pmcx_axi_noc0);
+	register_dev(60, &rp_pmc_npi);
+
+	register_dev(80, &rp_pl2ps_irq);
+	register_dev(83, &rp_wires_out);
+
+	register_dev(84, &rp_s_pl_axi_fpd3);
+
+	register_dev(85, &rp_m_fpd_axi_noc6);
+	register_dev(86, &rp_m_fpd_axi_noc7);
+	register_dev(87, &rp_m_fpd_axi_noc_iso);
+
+	register_dev(88, &rp_m_cpm_axi_noc2);
+	register_dev(89, &rp_m_cpm_axi_noc3);
+
+	register_dev(90, &rp_m_cpm);
+	register_dev(91, &rp_s_cpm);
+
+	register_dev(92, &rp_m_hnic);
+}
---
+++ soc/xilinx/versal-net/xilinx-versal-net.h
@@ -0,0 +1,155 @@
+/*
+ * Xilinx SystemC/TLM-2.0 Versal Net Wrapper.
+ *
+ * Written by Francisco Iglesias <francisco.iglesias@amd.com>
+ *
+ * Copyright (c) 2022, Xilinx Inc.
+ * All rights reserved.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
+ * THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+#ifndef __XILINX_VERSAL_NET_H__
+#define __XILINX_VERSAL_NET_H__
+
+#include "systemc.h"
+
+#include "tlm_utils/simple_initiator_socket.h"
+#include "tlm_utils/simple_target_socket.h"
+#include "tlm_utils/tlm_quantumkeeper.h"
+
+#include "remote-port-tlm.h"
+#include "remote-port-tlm-memory-master.h"
+#include "remote-port-tlm-memory-slave.h"
+#include "remote-port-tlm-wires.h"
+
+class xilinx_versal_net
+: public remoteport_tlm
+{
+private:
+	remoteport_tlm_memory_master rp_m_fpd_axi_pl;
+	remoteport_tlm_memory_master rp_m_lpd_axi_pl;
+
+	remoteport_tlm_memory_master rp_m_fpd_axi_noc0;
+	remoteport_tlm_memory_master rp_m_fpd_axi_noc1;
+	remoteport_tlm_memory_master rp_m_fpd_axi_noc2;
+	remoteport_tlm_memory_master rp_m_fpd_axi_noc3;
+	remoteport_tlm_memory_master rp_m_fpd_axi_noc4;
+	remoteport_tlm_memory_master rp_m_fpd_axi_noc5;
+	remoteport_tlm_memory_master rp_m_fpd_axi_noc6;
+	remoteport_tlm_memory_master rp_m_fpd_axi_noc7;
+	remoteport_tlm_memory_master rp_m_fpd_axi_noc_iso;
+
+	remoteport_tlm_memory_master rp_m_cpm_axi_noc0;
+	remoteport_tlm_memory_master rp_m_cpm_axi_noc1;
+	remoteport_tlm_memory_master rp_m_cpm_axi_noc2;
+	remoteport_tlm_memory_master rp_m_cpm_axi_noc3;
+
+	remoteport_tlm_memory_master rp_m_lpd_axi_noc0;
+	remoteport_tlm_memory_master rp_reserved_0;
+
+	remoteport_tlm_memory_master rp_m_pmcx_axi_noc0;
+	remoteport_tlm_memory_master rp_pmc_npi;
+
+	remoteport_tlm_memory_master rp_m_cpm;
+	remoteport_tlm_memory_master rp_m_hnic;
+
+	remoteport_tlm_memory_slave rp_s_pl_chi_fpd;
+
+	remoteport_tlm_memory_slave rp_s_pl_axi_fpd0;
+	remoteport_tlm_memory_slave rp_s_pl_axi_fpd1;
+	remoteport_tlm_memory_slave rp_s_pl_axi_fpd2;
+	remoteport_tlm_memory_slave rp_s_pl_axi_fpd3;
+
+	remoteport_tlm_memory_slave rp_s_noc_axi_fpd0;
+	remoteport_tlm_memory_slave rp_s_noc_axi_fpd1;
+	remoteport_tlm_memory_slave rp_s_noc_axi_fpd2;
+	remoteport_tlm_memory_slave rp_s_noc_axi_fpd3;
+
+	remoteport_tlm_memory_slave rp_s_pl_acp_apu;
+	remoteport_tlm_memory_slave rp_s_cpm;
+
+	remoteport_tlm_memory_slave rp_s_noc_axi_cpm;
+	remoteport_tlm_memory_slave rp_s_pl_axi_lpd;
+
+	remoteport_tlm_memory_slave rp_s_noc_axi_pmcx0;
+
+	remoteport_tlm_wires rp_pl2ps_irq;
+	remoteport_tlm_wires rp_wires_out;
+
+public:
+	/* FPD */
+	tlm_utils::simple_initiator_socket<remoteport_tlm_memory_master> *m_fpd_axi_pl;
+	/* LPD */
+	tlm_utils::simple_initiator_socket<remoteport_tlm_memory_master> *m_lpd_axi_pl;
+
+	tlm_utils::simple_initiator_socket<remoteport_tlm_memory_master> *m_fpd_axi_noc0;
+	tlm_utils::simple_initiator_socket<remoteport_tlm_memory_master> *m_fpd_axi_noc1;
+	tlm_utils::simple_initiator_socket<remoteport_tlm_memory_master> *m_fpd_axi_noc2;
+	tlm_utils::simple_initiator_socket<remoteport_tlm_memory_master> *m_fpd_axi_noc3;
+	tlm_utils::simple_initiator_socket<remoteport_tlm_memory_master> *m_fpd_axi_noc4;
+	tlm_utils::simple_initiator_socket<remoteport_tlm_memory_master> *m_fpd_axi_noc5;
+	tlm_utils::simple_initiator_socket<remoteport_tlm_memory_master> *m_fpd_axi_noc6;
+	tlm_utils::simple_initiator_socket<remoteport_tlm_memory_master> *m_fpd_axi_noc7;
+	tlm_utils::simple_initiator_socket<remoteport_tlm_memory_master> *m_fpd_axi_noc_iso;
+
+	tlm_utils::simple_initiator_socket<remoteport_tlm_memory_master> *m_cpm_axi_noc0;
+	tlm_utils::simple_initiator_socket<remoteport_tlm_memory_master> *m_cpm_axi_noc1;
+	tlm_utils::simple_initiator_socket<remoteport_tlm_memory_master> *m_cpm_axi_noc2;
+	tlm_utils::simple_initiator_socket<remoteport_tlm_memory_master> *m_cpm_axi_noc3;
+
+	tlm_utils::simple_initiator_socket<remoteport_tlm_memory_master> *m_lpd_axi_noc0;
+	tlm_utils::simple_initiator_socket<remoteport_tlm_memory_master> *s_reserved_0;
+
+	tlm_utils::simple_initiator_socket<remoteport_tlm_memory_master> *m_pmcx_axi_noc0;
+	tlm_utils::simple_initiator_socket<remoteport_tlm_memory_master> *pmc_npi;
+
+	tlm_utils::simple_initiator_socket<remoteport_tlm_memory_master> *m_cpm;
+	tlm_utils::simple_initiator_socket<remoteport_tlm_memory_master> *m_hnic;
+
+	/* FPD */
+	tlm_utils::simple_target_socket<remoteport_tlm_memory_slave> *s_pl_chi_fpd;
+
+	tlm_utils::simple_target_socket<remoteport_tlm_memory_slave> *s_pl_axi_fpd0;
+	tlm_utils::simple_target_socket<remoteport_tlm_memory_slave> *s_pl_axi_fpd1;
+	tlm_utils::simple_target_socket<remoteport_tlm_memory_slave> *s_pl_axi_fpd2;
+	tlm_utils::simple_target_socket<remoteport_tlm_memory_slave> *s_pl_axi_fpd3;
+
+	tlm_utils::simple_target_socket<remoteport_tlm_memory_slave> *s_noc_axi_fpd0;
+	tlm_utils::simple_target_socket<remoteport_tlm_memory_slave> *s_noc_axi_fpd1;
+	tlm_utils::simple_target_socket<remoteport_tlm_memory_slave> *s_noc_axi_fpd2;
+	tlm_utils::simple_target_socket<remoteport_tlm_memory_slave> *s_noc_axi_fpd3;
+
+	tlm_utils::simple_target_socket<remoteport_tlm_memory_slave> *s_pl_acp_apu;
+	tlm_utils::simple_target_socket<remoteport_tlm_memory_slave> *s_cpm;
+
+	/* LPD */
+	tlm_utils::simple_target_socket<remoteport_tlm_memory_slave> *s_noc_axi_cpm;
+	tlm_utils::simple_target_socket<remoteport_tlm_memory_slave> *s_pl_axi_lpd;
+
+	tlm_utils::simple_target_socket<remoteport_tlm_memory_slave> *s_noc_axi_pmcx0;
+
+	sc_vector<sc_signal<bool> > pl2ps_irq;
+	sc_vector<sc_signal<bool> > pl_reset;
+
+	xilinx_versal_net(sc_core::sc_module_name name, const char *sk_descr,
+				Iremoteport_tlm_sync *sync = NULL,
+				bool blocking_socket = true);
+};
+
+#endif
